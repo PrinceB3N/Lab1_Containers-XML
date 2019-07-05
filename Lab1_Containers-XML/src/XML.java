1a794a24e1c308797095a2b14224d9db82d44d45
benja <benja@DESKTOP-95GMM0O.attlocal.net>
2019-07-04 17:27:38 -0700

    Fixed code and product maps. Now need to fix method to read carts.xml and converting bitset to binary string.

--- Lab1_Containers-XML/src/XML.java
+++ Lab1_Containers-XML/src/XML.java
@@ -17,38 +17,47 @@ import org.xml.sax.SAXException;
 public class XML {
     private HashMap<BitSet,String> codeMap;
     private HashMap<BitSet,Float> productMap;
-    
+    private static final int codeLength=9;
     public XML(String codefilename, String xml_binaryname,String xml_charname, String productfilename, String xml_prodname, String xml_pricename) throws ParserConfigurationException,SAXException,IOException{
         codeMap=this.getCodeMapFrom(codefilename,xml_binaryname,xml_charname);
         productMap=this.getProductMapFrom(productfilename, xml_prodname, xml_pricename);
+ 
+        Iterator it = productMap.entrySet().iterator();
+        while (it.hasNext()) {
+            HashMap.Entry pair = (HashMap.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            //it.remove(); // avoids a ConcurrentModificationException
+        }
     }
     private HashMap<BitSet,String> getCodeMapFrom(String sfilename,String binary,String character) throws ParserConfigurationException,SAXException,IOException{
         HashMap<BitSet,String> hSet=new HashMap<BitSet,String>();
         XMLHandler xml = new XMLHandler();
         Document doc = xml.ReadXML(sfilename);
-        NodeList nList = xml.GetNodes(doc, sfilename);
+        NodeList nList = xml.GetNodes(doc, "Symbol");
+        
         for(int i=0;i<nList.getLength();i++){
             Node nNode=nList.item(i);
             if(nNode.getNodeType()==Node.ELEMENT_NODE){
                 Element eElement = (Element) nNode;
-                BitSet bSet =  BitSet.valueOf(eElement.getElementsByTagName(binary).item(0).getTextContent().getBytes());
+                BitSet bSet =  fromString(eElement.getElementsByTagName(binary).item(0).getTextContent());
                 String c = (eElement.getElementsByTagName(character).item(0).getTextContent());
                 hSet.put(bSet,c);
                     
             }
         }
+ 
         return hSet; 
     }
     private HashMap<BitSet,Float> getProductMapFrom(String sfilename,String prodname,String pricename) throws ParserConfigurationException,SAXException,IOException{
         HashMap<BitSet,Float> hSet=new HashMap<BitSet,Float>();
         XMLHandler xml = new XMLHandler();
         Document doc = xml.ReadXML(sfilename);
-        NodeList nList = xml.GetNodes(doc, sfilename);
+        NodeList nList = xml.GetNodes(doc, "Product");
         for(int i=0;i<nList.getLength();i++){
             Node nNode=nList.item(i);
             if(nNode.getNodeType()==Node.ELEMENT_NODE){
                 Element eElement = (Element) nNode;
-                BitSet bSet =  BitSet.valueOf(eElement.getElementsByTagName(prodname).item(0).getTextContent().getBytes());
+                BitSet bSet =  fromString(eElement.getElementsByTagName(prodname).item(0).getTextContent());
                 Float f = Float.parseFloat(eElement.getElementsByTagName(pricename).item(0).getTextContent());
                 if(!hSet.containsKey(bSet)){
                     hSet.put(bSet,f);
@@ -58,10 +67,46 @@ public class XML {
 
         return hSet;
     }
-    private void printReceipt(ArrayList<BitSet> cartList){
+    
+    private String deCodeProductName(BitSet bSet){
+        String codeName = bSet.toString();
+        String uncoded="";
+        for(int i=0;i<codeName.length()/codeLength;i++){
+            BitSet temp = fromString(codeName.substring(i*codeLength, (i+1)*codeLength));
+            uncoded+=codeMap.get(temp);
+        }
         
+        return uncoded;
     }
-    public void processCart(String sfilename, String codesfilename, String productsfilename, String itemname) throws ParserConfigurationException,SAXException,IOException{
+    private float getPrice(BitSet bSet){
+        return productMap.get(bSet);
+    }
+    private static BitSet fromString(String binary) {
+        BitSet bitset = new BitSet(binary.length());
+        int len = binary.length();
+        for (int i = len-1; i >= 0; i--) {
+            if (binary.charAt(i) == '1') {
+                bitset.set(len-i-1);
+            }
+        }
+        return bitset;
+    }
+    private void printReceipt(ArrayList<BitSet> cartList){
+        float total = 0.0f;
+        System.out.println("Printing Receipt...");
+        for(BitSet i:cartList){
+            System.out.println("Barcode Label: "+ i.toString());
+            System.out.println("Product Name: "+this.deCodeProductName(i));
+            System.out.println("Price: "+this.getPrice(i));
+            System.out.println("----------------------");
+            
+            total+=this.getPrice(i);
+        }
+        
+        System.out.println("\n\nTotal: "+total);
+   
+    }
+    public void processCarts(String sfilename, String itemname) throws ParserConfigurationException,SAXException,IOException{
             
             XMLHandler xml = new XMLHandler();
             Document doc = xml.ReadXML(sfilename);
@@ -76,27 +121,23 @@ public class XML {
                         Node innerNode = innerList.item(x);
                         if(nNode.getNodeType()==Node.ELEMENT_NODE){
                             Element eElement = (Element) nNode;
-                            cartList.add(BitSet.valueOf(eElement.geitem(0).getTextContent().getBytes()));
+                            cartList.add(BitSet.valueOf(eElement.getElementsByTagName("Item").item(0).getTextContent().getBytes()));
                         }
                         
                     }
-                    
+                    System.out.println("Printing Receipt for Cart "+i+"...");
                     this.printReceipt(cartList);
                 }
-            }
-            
-               
+            }           
     }
 public static void main(String[] args)
 {
-    String sfilename = "Passengers.XML";
-    String snode = "Passenger";
-    String[] sfields = new String[]{"Row","Seat","Name"};
-    XMLHandler xml;
-    Document doc;
+    String codeFile = "./src/Barcodes3of9.xml";
+    String productFile = "./src/BCProducts.xml";
+    String cartsFile = "./src/Carts.xml";
     try{
-        xml = new XMLHandler();
-        doc = xml.ReadXML(sfilename);
+        XML test= new XML(codeFile, "Binary", "Character", productFile, "Barcode", "Price");
+        test.processCarts(cartsFile, "item");
     }
     catch (ParserConfigurationException e) 
     {
